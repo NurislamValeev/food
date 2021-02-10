14d718f4a06ab2069f361fb884da5bf952936bc0
Nurislam <nurislamvaleev@gmail.com>
2021-02-10 15:41:55 +0300

    added spinner, server.php and good-looking notification after filling the form

--- js/script.js
+++ js/script.js
@@ -101,7 +101,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const modalWindow = document.querySelector('.modal');
 	const contactUsBtns = document.querySelectorAll('[data-modal]');
-	const modalClose = document.querySelector('[data-close]');
 
 	function openModal() {
 		modalWindow.classList.add('show', 'fade-fast');
@@ -120,10 +119,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		document.body.style.overflow = '';
 	}
 
-	modalClose.addEventListener('click', closeModal);
-
 	modalWindow.addEventListener('click', (event) => {
-		if (event.target === modalWindow) {
+		if (
+			event.target === modalWindow ||
+			event.target.getAttribute('data-close') == ''
+		) {
 			closeModal();
 		}
 	});
@@ -134,7 +134,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	// const modalTimerId = setTimeout(openModal, 5000);
+	const modalTimerId = setTimeout(openModal, 50000);
 
 	function showModalByScroll() {
 		if (
@@ -151,12 +151,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Используем классы для карточек
 
 	class MenuCard {
-		constructor(src, alt, title, descr, price, parentSelector) {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
 			this.src = src;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 27;
 			this.changeToUAH();
@@ -168,16 +169,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach((className) => element.classList.add(className));
+			}
+
 			element.innerHTML = `
-				<div class="menu__item">
-					<img src=${this.src} alt=${this.alt} />
-					<h3 class="menu__item-subtitle">${this.title}</h3>
-					<div class="menu__item-descr">${this.descr}</div>
-					<div class="menu__item-divider"></div>
-					<div class="menu__item-price">
-						<div class="menu__item-cost">Цена:</div>
-						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-					</div>
+				<img src=${this.src} alt=${this.alt} />
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 				</div>
 			`;
 			this.parent.append(element);
@@ -210,7 +217,86 @@ window.addEventListener('DOMContentLoaded', () => {
 		21,
 		'.menu .container'
 	).render();
+
+	// Forms
+
+	const forms = document.querySelectorAll('form');
+	const message = {
+		loading: 'img/form/spinner.svg',
+		success: 'Спасибо! Скоро мы с Вами свяжемся.',
+		failure: 'Что-то пошло не так...',
+	};
+
+	forms.forEach((itemForm) => {
+		postData(itemForm);
+	});
+
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const statusMessage = document.createElement('img');
+			statusMessage.src = message.loading;
+			statusMessage.style.cssText = `
+				display: block;
+				margin: 0 auto;
+			`;
+			form.insertAdjacentElement('afterend', statusMessage);
+
+			const request = new XMLHttpRequest();
+
+			request.open('POST', 'server.php');
+			request.setRequestHeader('Content-type', 'application/json');
+
+			const formData = new FormData(form);
+
+			const object = {};
+			formData.forEach(function (value, key) {
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+
+			request.send(json);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					showThanksModal(message.success);
+					form.reset();
+					statusMessage.remove();
+				} else {
+					showThanksModal(message.failure);
+				}
+			});
+		});
+	}
+
+	function showThanksModal(message) {
+		const prevModalDialog = document.querySelector('.modal__dialog');
+		prevModalDialog.classList.add('hide');
+		openModal();
+
+		const thanksModal = document.createElement('div');
+		thanksModal.classList.add('modal__dialog');
+		thanksModal.innerHTML = `
+		<div class="modal__content">
+			<div class="modal__close" data-close>&times;</div>
+			<div class="modal__title">${message}</div>
+		</div>
+		`;
+
+		document.querySelector('.modal').append(thanksModal);
+		setTimeout(() => {
+			thanksModal.remove();
+			prevModalDialog.classList.add('show');
+			prevModalDialog.classList.remove('hide');
+			closeModal();
+		}, 4000);
+	}
 });
+
+//=====================================================================================================
 
 // function User(name, id) {
 // 	this.name = name;
@@ -304,12 +390,12 @@ console.log(double(13));
 
 //Дабл - это новая функция, у которой есть привязанный контекст.
 
-const newBtn = document.querySelector('.this_btn');
+// const newBtn = document.querySelector('.this_btn');
 
-newBtn.addEventListener('click', function () {
-	console.log(this); // контекст вызова - сам элемент, на котором произошло событие
-	this.style.backgroundColor = 'red';
-});
+// newBtn.addEventListener('click', function () {
+// 	console.log(this); // контекст вызова - сам элемент, на котором произошло событие
+// 	this.style.backgroundColor = 'red';
+// });
 
 //=========================================================================
 
@@ -344,3 +430,11 @@ console.log(div.calcArea());
 
 // console.log(square.calcArea());
 // console.log(long.calcArea());
+
+//====================================================================================================
+
+const log = function (a, b, ...rest) {
+	console.log(a, b, rest);
+};
+
+log('basic', 'rest', 'operator', 'usage', 'for sure!');
