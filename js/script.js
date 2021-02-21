f110c88e21a27c90811c967c55712c038940bd28
Nurislam <nurislamvaleev@gmail.com>
2021-02-21 15:24:05 +0300

    added db.json

--- js/script.js
+++ js/script.js
@@ -151,12 +151,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Используем классы для карточек
 
 	class MenuCard {
-		constructor(src, alt, title, descr, price, parentSelector) {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
 			this.src = src;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 27;
 			this.changeToUAH();
@@ -168,16 +169,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach((className) => element.classList.add(className));
+			}
+
 			element.innerHTML = `
-				<div class="menu__item">
-					<img src=${this.src} alt=${this.alt} />
-					<h3 class="menu__item-subtitle">${this.title}</h3>
-					<div class="menu__item-descr">${this.descr}</div>
-					<div class="menu__item-divider"></div>
-					<div class="menu__item-price">
-						<div class="menu__item-cost">Цена:</div>
-						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-					</div>
+				<img src=${this.src} alt=${this.alt} />
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 				</div>
 			`;
 			this.parent.append(element);
@@ -211,136 +218,3 @@ window.addEventListener('DOMContentLoaded', () => {
 		'.menu .container'
 	).render();
 });
-
-// function User(name, id) {
-// 	this.name = name;
-// 	this.id = id;
-// 	this.human = true;
-// 	this.hello = function () {
-// 		console.log(`Hello, ${this.name}`);
-// 	};
-// }
-
-// User.prototype.exit = function () {
-// 	console.log(`Пользователь ${this.name} ушел`);
-// };
-
-// const ivan = new User('Ivan', 28);
-// const alex = new User('Alex', 20);
-
-// ivan.exit();
-
-// ivan.hello();
-// alex.hello();
-
-// console.log(ivan);
-// console.log(alex);
-
-//========================================================================
-
-// function showThis(a, b) {
-// 	console.log(this);
-
-// 	function sum() {
-// 		console.log(this);
-// 		return a + b;
-// 	}
-
-// 	console.log(sum());
-// }
-
-// showThis(4, 5);
-
-// const obj = {
-// 	a: 20,
-// 	b: 15,
-// 	sum: function () {
-// 		function shout() {
-// 			console.log(this);
-// 		}
-// 		shout();
-// 	},
-// };
-
-// obj.sum();
-
-// function User(name, id) {
-// 	this.name = name;
-// 	this.id = id;
-// 	this.human = true;
-// 	this.hello = function () {
-// 		console.log('Hello! ' + this.name);
-// 	};
-// }
-
-// let ivan = new User('Ivan', 23);
-
-function sayName() {
-	console.log(this);
-	console.log(this.name);
-}
-
-const user = {
-	name: 'John',
-};
-
-sayName.call(user); // во внутрь мы передаем контекст вызова,
-// который мы хотим передать в эту функцию.
-
-// тоже самое будет и с apply.
-
-sayName.apply(user);
-
-function count(num) {
-	return this * num; // здесь не хватает контекста вызова
-}
-
-const double = count.bind(2);
-console.log(double(3));
-console.log(double(13));
-
-// В эту переменную мы помещаем новую функцию!
-// Число 2 переходит в this!
-
-//Дабл - это новая функция, у которой есть привязанный контекст.
-
-const newBtn = document.querySelector('.this_btn');
-
-newBtn.addEventListener('click', function () {
-	console.log(this); // контекст вызова - сам элемент, на котором произошло событие
-	this.style.backgroundColor = 'red';
-});
-
-//=========================================================================
-
-class Rectangle {
-	constructor(height, width) {
-		this.height = height;
-		this.width = width;
-	}
-
-	calcArea() {
-		return this.height * this.width;
-	}
-}
-
-class ColoredRectangleWithText extends Rectangle {
-	constructor(height, width, text, bgColor) {
-		super(height, width);
-		this.text = text;
-		this.bgColor = bgColor;
-	}
-
-	showMyProps() {
-		console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-	}
-}
-
-const div = new ColoredRectangleWithText(25, 10, 'Hello World!', 'red');
-div.showMyProps();
-console.log(div.calcArea());
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-
-// console.log(square.calcArea());
-// console.log(long.calcArea());
