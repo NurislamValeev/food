020d13749b3a5508a4da79f3603eb5a99c99427d
Nurislam <nurislamvaleev@gmail.com>
2021-02-22 14:38:32 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -189,32 +189,41 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	new MenuCard(
-		'img/tabs/vegy.jpg',
-		'vegy',
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: большесвежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container'
-	).render();
+	const getResource = async (url) => {
+		const res = await fetch(url);
 
-	new MenuCard(
-		'img/tabs/elite.jpg',
-		'elite',
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		14,
-		'.menu .container'
-	).render();
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-	new MenuCard(
-		'img/tabs/post.jpg',
-		'post',
-		'Меню "Постное"',
-		'“Постное меню” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения. Полная гармония с собой и природой в каждом элементе! Все будет Ом!',
-		21,
-		'.menu .container'
-	).render();
+		return await res.json();
+	};
+
+	// getResource('http://localhost:3000/menu').then((data) => {
+	// data.forEach(({ img, altimg, title, descr, price }) => {
+	// 	new MenuCard(
+	// 		img,
+	// 		altimg,
+	// 		title,
+	// 		descr,
+	// 		price,
+	// 		'.menu .container'
+	// 	).render();
+	// });
+	// });
+
+	axios.get('http://localhost:3000/menu').then((data) => {
+		data.data.forEach(({ img, altimg, title, descr, price }) => {
+			new MenuCard(
+				img,
+				altimg,
+				title,
+				descr,
+				price,
+				'.menu .container'
+			).render();
+		});
+	});
 
 	// Forms
 
@@ -225,11 +234,21 @@ window.addEventListener('DOMContentLoaded', () => {
 		failure: 'Что-то пошло не так...',
 	};
 
-	forms.forEach((itemForm) => {
-		postData(itemForm);
+	forms.forEach((item) => {
+		bindPostData(item);
 	});
 
-	function postData(form) {
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: data,
+		});
+
+		return await res.json();
+	};
+
+	function bindPostData(form) {
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
 
@@ -243,17 +262,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			const formData = new FormData(form);
 
-			const object = {};
-			formData.forEach(function (value, key) {
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			fetch('server.php', {
-				method: 'POST',
-				headers: { 'Content-type': 'application/json' },
-				body: JSON.stringify(object),
-			})
-				.then((data) => data.text())
+			postData('http://localhost:3000/requests', json)
 				.then((data) => {
 					console.log(data);
 					showThanksModal(message.success);
@@ -291,15 +302,58 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 4000);
 	}
 
-	// fetch('https://jsonplaceholder.typicode.com/posts', {
-	// 	method: 'POST',
-	// 	body: JSON.stringify({ name: 'Alex' }),
-	// 	headers: {
-	// 		'Content-type': 'application/json',
-	// 	},
-	// })
-	// 	.then((response) => response.json())
-	// 	.then((json) => console.log(json));
+	// Slider
+
+	const slides = document.querySelectorAll('.offer__slide');
+	const prev = document.querySelector('.offer__slider-prev');
+	const next = document.querySelector('.offer__slider-next');
+
+	const current = document.querySelector('#current');
+	const total = document.querySelector('#total');
+
+	let slideIndex = 1;
+
+	showSlides(slideIndex);
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+	} else {
+		total.textContent = slides.length;
+	}
+
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
+
+		slides.forEach((item) => item.classList.add('hide'));
+		slides.forEach((item) => item.classList.remove('show', 'fade'));
+
+		slides[slideIndex - 1].classList.add('show', 'fade');
+		slides[slideIndex - 1].classList.remove('hide');
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	}
+
+	function plusSlides(n) {
+		showSlides((slideIndex += n));
+	}
+
+	prev.addEventListener('click', () => {
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', () => {
+		plusSlides(1);
+	});
 });
 
 //=====================================================================================================
--- js/tests.js
+++ js/tests.js
@@ -49,12 +49,64 @@ const test = (time) => {
 // test(1000).then(() => console.log('1000 ms'));
 // test(2000).then(() => console.log('2000 ms'));
 
-Promise.all([test(1000), test(2000)]).then(() => {
-	console.log('All');
-});
+// Promise.all([test(1000), test(2000)]).then(() => {
+// 	console.log('All');
+// });
 
-Promise.race([test(1000), test(2000)]).then(() => {
-	console.log('All');
-});
+// Promise.race([test(1000), test(2000)]).then(() => {
+// 	console.log('All');
+// });
 
-// ========Методы перебора массивов========
+// ======== Методы перебора массивов ========
+
+// filter
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+
+// const shortNames = names.filter(function (name) {
+// 	return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+// ============ map ==================
+
+// let answers = ['IvAn', 'AnnA', 'HElLO'];
+
+// answers = answers.map((item) => item.toLowerCase());
+
+// console.log(answers);
+
+//========== every/some ===============
+
+// const some = [4, 'eqwewqf', 'eqweq23f'];
+
+// console.log(some.some((item) => typeof item === 'number'));
+// console.log(some.every((item) => typeof item === 'number'));
+
+//================ reduce =====================
+
+// const arr = [4, 5, 1, 3, 2, 6];
+
+// const res = arr.reduce((sum, current) => sum + current);
+
+// console.log(res);
+
+// const arr = ['apple', 'banana', 'strawberry'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+// console.log(res);
+
+const obj = {
+	ivan: 'person',
+	ann: 'person',
+	dog: 'animal',
+	cat: 'animal',
+};
+
+const newArr = Object.entries(obj)
+	.filter((item) => item[1] === 'person')
+	.map((item) => item[0]);
+
+console.log(newArr);
